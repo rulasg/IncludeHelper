b5634ea39214fef1a0939e9d329030a9322a56a7
rulasg <rulasg@github.com>
2025-03-09 21:57:54 +0100

    Add Get-HashCode function to compute MD5 hash of input strings with corresponding test

---
+++ Test/public/include.getHashCode.test.ps1
@@ -0,0 +1,9 @@
+function Test_GetHashCode{
+
+    # Load include file to test
+    . $(Get-Ps1FullPath -Name getHashCode.ps1 -FolderName "Include")
+
+    $result = Get-HashCode -InputString "test"
+
+    Assert-AreEqual -Expected "098F6BCD4621D373CADE4E832627B4F6" -Presented $result
+}
