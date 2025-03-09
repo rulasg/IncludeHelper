adc2335ff3be0a69f2953ffb4864477c9b01396d
rulasg <rulasg@github.com>
2025-03-09 22:28:13 +0100

    Add database mock implementation and associated test functions for improved testing capabilities

---
+++ Test/include/database.mock.ps1
@@ -0,0 +1,47 @@
+# DATABASE MOCK 
+#
+# This file is used to mock the database path and the database file
+# for the tests. It creates a mock database path and a mock database file
+# and sets the database path to the mock database path.
+# We need to define variables for this include to work
+# $MOCK_DATABASE_PATH : The path used as the mock database folder
+# $DB_INVOKE_GET_ROOT_PATH_CMD : Invoke command that is needed to be mocked
+# $DB_INVOKE_GET_ROOT_PATH_ALIAS : Invoke function to retreive the root path of the database
+#
+# Sample file
+# # DATABASE MOCK VARIABLES
+# # This file is required for DATABASE MOCK to work
+# $DB_INVOKE_GET_ROOT_PATH_CMD = "Invoke-MyModuleGetDbRootPath"
+# $DB_INVOKE_GET_ROOT_PATH_ALIAS = "MyModuleGetDbRootPath"
+#
+# $MOCK_DATABASE_PATH = "test_database_path"
+
+function Mock_Database([switch]$ResetDatabase){
+
+    MockCallToString $DB_INVOKE_GET_ROOT_PATH_CMD -OutString "test_database_path"
+
+    $dbstore = Invoke-MyCommand -Command $DB_INVOKE_GET_ROOT_PATH_CMD
+    Assert-AreEqual -Expected "test_database_path" -Presented $dbstore
+
+    if($ResetDatabase){
+        Reset-DatabaseStore
+    }
+
+}
+
+function Get-Mock_DatabaseStore{
+    $dbstore = Invoke-MyCommand -Command $DB_INVOKE_GET_ROOT_PATH_CMD
+    return $dbstore
+}
+
+function Reset-DatabaseStore{
+    [CmdletBinding()]
+    param()
+
+        $databaseRoot = Invoke-MyCommand -Command $DB_INVOKE_GET_ROOT_PATH_ALIAS
+    
+        Remove-Item -Path $databaseRoot -Recurse -Force -ErrorAction SilentlyContinue
+
+        New-Item -Path $databaseRoot -ItemType Directory
+
+}
---
+++ Test/public/include.databasev2.test.ps1
@@ -0,0 +1,91 @@
+
+function Test_Database{
+
+    Reset-InvokeCommandMock
+    Mock_Database -ResetDatabase
+
+    # Load include file to test
+    . $(Get-Ps1FullPath -Name "databaseV2.ps1" -FolderName "Include")
+
+    # Get Default Database Root Path
+    $result = GetDatabaseRootPath
+    $expected = [System.Environment]::GetFolderPath('UserProfile') | Join-Path -ChildPath ".helpers" -AdditionalChildPath $moduleName, "databaseCache"
+    Assert-AreEqual -Expected $expected -Presented $result
+
+    # Get actual store path
+    $StorePath = Invoke-MyCommand -Command $DB_INVOKE_GET_ROOT_PATH_CMD
+    Assert-AreEqual -Expected "test_database_path" -Presented $StorePath
+    $items = Get-ChildItem -Path $result
+    Assert-Count -Expected 0 -Presented $items
+
+    # GetDatabaseFile
+    $result = GetDatabaseFile -Key "test"
+    Assert-AreEqual -Expected "test_database_path/test.json" -Presented $result
+
+    # Test if database is empty
+    $result = Test-DatabaseKey -Key "test"
+    Assert-IsFalse -Condition $result
+    $result = Get-DatabaseKey -Key "test"
+    Assert-IsNull -Object $result
+
+    # Save content to database
+    Save-DatabaseKey -Key "test" -Value "dummy content"
+    $result = Test-DatabaseKey -Key "test"
+    Assert-IsTrue -Condition $result
+
+    $result = Get-DatabaseKey -Key "test"
+    Assert-AreEqual -Expected "dummy content" -Presented $result
+
+    # Check the number of files in store
+    $items = Get-ChildItem -Path $StorePath
+    Assert-Count -Expected 1 -Presented $items
+
+    # Reset Database
+    Reset-DatabaseKey -Key "test"
+    $result = Test-DatabaseKey -Key "test"
+    Assert-IsFalse -Condition $result
+}
+
+function Test_Database_MultyKey{
+
+    Reset-InvokeCommandMock
+    Mock_Database -ResetDatabase
+
+    # Load include file to test
+    . $(Get-Ps1FullPath -Name "databaseV2.ps1" -FolderName "Include")
+
+    # Add several keys
+    Save-DatabaseKey -Key "test1" -Value "dummy content 1"
+    Save-DatabaseKey -Key "test2" -Value "dummy content 2"
+    Save-DatabaseKey -Key "test3" -Value "dummy content 3"
+    Save-DatabaseKey -Key "test4" -Value "dummy content 4"
+    Save-DatabaseKey -Key "test5" -Value "dummy content 5"
+
+    # Check the number of files in store
+    $StorePath = Invoke-MyCommand -Command $DB_INVOKE_GET_ROOT_PATH_CMD
+    $items = Get-ChildItem -Path $StorePath
+    Assert-Count -Expected 5 -Presented $items
+
+    # Get one key from the middle
+    $result = Get-DatabaseKey -Key "test4"
+    Assert-AreEqual -Expected "dummy content 4" -Presented $result
+
+    # Test one key from the middle
+    $result = Test-DatabaseKey -Key "test4"
+    Assert-IsTrue -Condition $result
+
+    # Test a key that does not exists
+    $result = Test-DatabaseKey -Key "test6"
+    Assert-IsFalse -Condition $result
+
+    # Reset a key that does not exists
+    $result = Reset-DatabaseKey -Key "test6"
+    Assert-IsNull -Object $result
+
+    # Reset a key that exists
+    $result = Reset-DatabaseKey -Key "test4"
+    Assert-IsNull -Object $result
+    $result = Test-DatabaseKey -Key "test4"
+    Assert-IsFalse -Condition $result
+
+}
