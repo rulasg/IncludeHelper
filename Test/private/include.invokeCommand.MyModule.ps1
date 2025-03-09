68d5964daa32ce6dc6e2e64bce2723d9cb32579f
rulasg <rulasg@github.com>
2025-03-09 22:00:48 +0100

    Add mock functions for InvokeCommand to facilitate testing and improve command alias handling

---
+++ Test/private/include.invokeCommand.MyModule.ps1
@@ -0,0 +1,4 @@
+
+$MODULE_INVOKATION_TAG = "MyModuleHelperModule"
+$MODULE_INVOKATION_TAG_MOCK = "MyModuleHelperModule-Mock"
+$MOCK_PATH = $PSScriptRoot | Split-Path -Parent | Join-Path -ChildPath 'private' -AdditionalChildPath 'mocks'
