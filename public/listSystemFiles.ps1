fb61682f1892e37080b5388769ca27529f2f2940
rulasg <rulasg@github.com>
2025-03-10 22:27:29 +0100

    Comment out unused include items in Get-IncludeSystemFiles function

--- public/listSystemFiles.ps1
+++ public/listSystemFiles.ps1
@@ -23,37 +23,37 @@ function Get-IncludeSystemFiles{
     $includeItems += [PSCustomObject]@{ FolderName = 'TestRoot' ; Name = 'Test.psd1' }
     $includeItems += [PSCustomObject]@{ FolderName = 'TestRoot' ; Name = 'Test.psm1' }
 
-    # TestInclude
-    $includeItems += [PSCustomObject]@{ FolderName = 'TestInclude' ; Name = 'config.mock.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'TestInclude' ; Name = 'database.mock.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'TestInclude' ; Name = 'invokeCommand.mock.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'TestInclude' ; Name = 'module.helper.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'TestInclude' ; Name = 'ps1.test.helper.ps1' }
+    # # TestInclude
+    # $includeItems += [PSCustomObject]@{ FolderName = 'TestInclude' ; Name = 'config.mock.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'TestInclude' ; Name = 'database.mock.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'TestInclude' ; Name = 'invokeCommand.mock.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'TestInclude' ; Name = 'module.helper.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'TestInclude' ; Name = 'ps1.test.helper.ps1' }
 
-    # TestPublic
-    $includeItems += [PSCustomObject]@{ FolderName = 'TestPublic' ; Name = 'include.config.test.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'TestPublic' ; Name = 'include.databasev2.test.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'TestPublic' ; Name = 'include.getHashCode.test.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'TestPublic' ; Name = 'include.module.helper.test.ps1' }
+    # # TestPublic
+    # $includeItems += [PSCustomObject]@{ FolderName = 'TestPublic' ; Name = 'include.config.test.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'TestPublic' ; Name = 'include.databasev2.test.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'TestPublic' ; Name = 'include.getHashCode.test.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'TestPublic' ; Name = 'include.module.helper.test.ps1' }
 
-    # TestPrivate
-    $includeItems += [PSCustomObject]@{ FolderName = 'TestPrivate' ; Name = 'include.config.mock.MyModule.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'TestPrivate' ; Name = 'include.database.mock.MyModule.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'TestPrivate' ; Name = 'include.invokeCommand.MyModule.ps1' }
+    # # TestPrivate
+    # $includeItems += [PSCustomObject]@{ FolderName = 'TestPrivate' ; Name = 'include.config.mock.MyModule.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'TestPrivate' ; Name = 'include.database.mock.MyModule.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'TestPrivate' ; Name = 'include.invokeCommand.MyModule.ps1' }
 
-    # Include
-    $includeItems += [PSCustomObject]@{ FolderName = 'Include' ; Name = 'config.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'Include' ; Name = 'databaseV2.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'Include' ; Name = 'getHashCode.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'Include' ; Name = 'mySetInvokeCommandAlias.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'Include' ; Name = 'ps1.helper.ps1' }
+    # # Include
+    # $includeItems += [PSCustomObject]@{ FolderName = 'Include' ; Name = 'config.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'Include' ; Name = 'databaseV2.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'Include' ; Name = 'getHashCode.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'Include' ; Name = 'mySetInvokeCommandAlias.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'Include' ; Name = 'ps1.helper.ps1' }
 
-    # Public
-    $includeItems += [PSCustomObject]@{ FolderName = 'Public' ; Name = 'include.config.MyModule.ps1' }
-    $includeItems += [PSCustomObject]@{ FolderName = 'Public' ; Name = 'include.databasev2.MyModule.ps1' }
+    # # Public
+    # $includeItems += [PSCustomObject]@{ FolderName = 'Public' ; Name = 'include.config.MyModule.ps1' }
+    # $includeItems += [PSCustomObject]@{ FolderName = 'Public' ; Name = 'include.databasev2.MyModule.ps1' }
 
-    # Private
-    $includeItems += [PSCustomObject]@{ FolderName = 'Private' ; Name = 'include.mySetInvokeCommandAlias.MyModule.ps1' }
+    # # Private
+    # $includeItems += [PSCustomObject]@{ FolderName = 'Private' ; Name = 'include.mySetInvokeCommandAlias.MyModule.ps1' }
 
     # DevContainer
     $includeItems += [PSCustomObject]@{ FolderName = 'DevContainer' ; Name = 'devcontainer.json' }
