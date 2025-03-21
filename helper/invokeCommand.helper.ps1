ba2e80160dd9a3d17761f60585caf52b9b7aba35
rulasg <rulasg@github.com>
2025-03-21 17:40:27 +0100

    refactor: remove unused MyModule script and simplify module root path logic

--- helper/invokeCommand.helper.ps1
+++ helper/invokeCommand.helper.ps1
@@ -7,7 +7,7 @@
 # Sample:
 # $MODULE_INVOKATION_TAG = "SfHelperModule"
 
-$moduleRootPath = $PSScriptRoot | Split-Path -Parent | Split-Path -Parent
+$moduleRootPath = $PSScriptRoot | Split-Path -Parent
 $MODULE_NAME = (Get-ChildItem -Path $moduleRootPath -Filter *.psd1 | Select-Object -First 1).BaseName
 $MODULE_INVOKATION_TAG = "$($MODULE_NAME)Module"
 
