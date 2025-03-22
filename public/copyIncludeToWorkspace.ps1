11813ad0ab96961e1d85c934117d58c6aaa3776e
rulasg <rulasg@github.com>
2025-03-22 11:22:16 +0100

    feat: add Copy-IncludeToWorkspace function and corresponding tests

---
+++ public/copyIncludeToWorkspace.ps1
@@ -0,0 +1,45 @@
+function Copy-IncludeToWorkSpace{
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        # Add relative path to the include file
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName,Position=0)][string]$FolderPath,
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName,Position=0)][string]$Name,
+        [Parameter()][string]$DestinationModulePath = "."
+    )
+    
+    process{
+
+        $includeRoot = Get-ModuleFolder -FolderName 'Root'
+
+        $sourceIncludeModuleFolder = $includeRoot | Join-Path -ChildPath $FolderPath
+        "Source folder is $sourceIncludeModuleFolder" | Write-Verbose
+        $destinationpath = $DestinationModulePath | Join-Path -ChildPath $FolderPath
+        "Destination folder is $destinationpath" | Write-Verbose
+        
+        $sourceFile = $sourceIncludeModuleFolder | Join-Path -ChildPath $Name
+        "Source file is $sourceFile" | Write-Verbose
+        $destinationFile = $destinationpath | Join-Path -ChildPath $Name
+        "Destination file is $destinationFile" | Write-Verbose
+        
+        # Check if there is a .psd1 file in the DestinationModulePath
+        $psd1File = Get-ChildItem -Path $DestinationModulePath -Filter *.psd1 -ErrorAction SilentlyContinue
+        if (-Not $psd1File) {
+            throw "Destination Path $DestinationModulePath does not seem to be a PowershellMddule."
+        }
+        
+        # create destination folder if it does not exist
+        if(-Not (Test-Path $destinationpath)){
+            $null = New-Item -Path $destinationpath -ItemType Directory -Force
+        }
+        
+        # Check if source file exists
+        if(-Not (Test-Path $sourceFile)){
+            throw "File $sourceFile not found"
+        }
+        
+        if ($PSCmdlet.ShouldProcess("$sourceFile", "copy to $destinationFile")) {
+            Copy-Item -Path $sourceFile -Destination $destinationFile -Force
+        }
+    }
+
+} Export-ModuleMember -Function Copy-IncludeToWorkSpace
