a4f6e936f80cf580b97d9ad9de1079b2102f5c34
rulasg <rulasg@github.com>
2025-03-26 07:33:31 +0100

    feat: add custom logging functions for error, warning, verbose, and host messages

---
+++ include/MyWrite.ps1
@@ -0,0 +1,43 @@
+
+$ERROR_COLOR = "Red"
+$WARNING_COLOR = "Yellow"
+$OUTPUT_COLOR = "DarkCyan"
+
+function Write-MyError{
+    param(
+        [Parameter(Mandatory,ValueFromPipeline)][string]$Message
+    )
+    # Write-Host "Error: $message" -ForegroundColor $ERROR_COLOR
+    Write-ToConsole $message -Color $ERROR_COLOR
+}
+
+function Write-MyWarning{
+    param(
+        [Parameter(Mandatory,ValueFromPipeline)][string]$Message
+    )
+    # Write-Host "Error: $message" -ForegroundColor $WARNING_COLOR
+    Write-ToConsole $message -Color $WARNING_COLOR
+}
+
+function Write-MyVerbose{
+    param(
+        [Parameter(ValueFromPipeline)][string]$Message
+    )
+    Write-Verbose -Message $message
+}
+
+function Write-MyHost{
+    param(
+        [Parameter(ValueFromPipeline)][string]$Message
+    )
+    # Write-Host $message -ForegroundColor $OUTPUT_COLOR
+    Write-ToConsole $message -Color $OUTPUT_COLOR
+}
+
+function Write-ToConsole{
+    param(
+        [Parameter(ValueFromPipeline)][ValidateSet("Red","Green","Yellow","Cyan","White", "Magenta")][string]$Color = "Magenta",
+        [Parameter(ValueFromPipeline, Position=0)][string]$Message
+    )
+    Write-Host $message -ForegroundColor $Color
+}
