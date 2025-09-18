e3cd23801d84dfc1e053add94c3903cdda77f2fe
Raúl (Dibildos) González <rulasg@github.com>
2025-09-18 09:30:00 +0200

    test(callPrivateFunction): assert private function errors outside context and can be invoked with Invoke-PrivateContext

--- Test/public/callPrivateFunction.test.ps1
+++ Test/public/callPrivateFunction.test.ps1
@@ -1,6 +1,23 @@
 function Test_InvokePrivateFunction{
 
-    Invoke-PrivateContext{
+    # First check that private function call fails
+    $errorStr = @"
+The term 'Get-PrivateString' is not recognized as a name of a cmdlet, function, script file, or executable program.
+Check the spelling of the name, or if a path was included, verify that the path is correct and try again.
+"@
+
+    $hasthrown = $false
+    try{
+        Get-PrivateString -Param1 "TestParam"
+    } catch {
+        $hasthrown = $true
+        Assert-AreEqual -Expected $errorStr -Presented $_.Exception.Message
+    }
+    Assert-IsTrue -Condition $hasthrown
+
+
+    # Call private function through Invoke-PrivateContext
+    Invoke-PrivateContext {
 
         $result = Get-PrivateString -Param1 "TestParam"
 
