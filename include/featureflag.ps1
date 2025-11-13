33b6f706c8d850555d8d6cc32387c7dc66e25044
Raúl (Dibildos) González <rulasg@github.com>
2025-11-13 09:44:10 +0100

    feat(featureflag): add module name handling and feature flag management functions

--- include/featureflag.ps1
+++ include/featureflag.ps1
@@ -8,6 +8,8 @@
 # features with less risk
 #
 
+$MODULE_NAME_PATH = ($PSScriptRoot | Split-Path -Parent | Get-ChildItem -Filter *.psd1 | Select-Object -First 1) | Split-Path -Parent
+$MODULE_NAME = $MODULE_NAME_PATH | Split-Path -LeafBase
 
 function Get-FeatureFlags{
     [CmdletBinding()]
@@ -83,4 +85,76 @@ function Clear-FeatureFlag{
 
     Set-FeatureFlag -Key $Key -Value $false
 
+}
+
+function Clear-FeatureFlagsRegistered{
+    [cmdletbinding()]
+    param()
+
+    $rffs = Get-ModuleNameRegisteredFeatureFlags
+    $ffs = (Get-FeatureFlags).Clone()
+
+    $rffs.deprecated | ForEach-Object {
+        $ffs.Remove($_)
+    }
+
+    Save-FeatureFlags $ffs
+
+}
+
+######
+
+function Get-ModuleNameRegisteredFeatureFlags{
+    [cmdletbinding()]
+    param()
+
+    $ffPath = $MODULE_NAME_PATH | Join-Path -ChildPath "featureflags.json"
+
+    if(! ($ffPath | Test-Path)){
+        return
+    }
+
+    $Json = Get-Content $ffPath 
+
+    $ff = $Json | ConvertFrom-Json
+
+    return $ff
+
+}
+$function = "Get-ModuleNameRegisteredFeatureFlags"
+$destFunction = $function -replace "ModuleName", $MODULE_NAME
+if( -not (Test-Path function:$destFunction )){
+    Copy-Item -path Function:$function -Destination Function:$destFunction
+    Export-ModuleMember -Function $destFunction
+}
+
+function Get-ModuleNameFeatureFlags{
+    [cmdletbinding()]
+    param()
+
+    $ffs = Get-FeatureFlags
+
+    return $ffs
+}
+$function = "Get-ModuleNameFeatureFlags"
+$destFunction = $function -replace "ModuleName", $MODULE_NAME
+if( -not (Test-Path function:$destFunction )){
+    Rename-Item -path Function:$function -NewName $destFunction
+    Export-ModuleMember -Function $destFunction
+}
+
+function Set-ModuleNameFeatureFlag{
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory,Position=0)][string]$Key,
+        [Parameter()][bool]$Value = $true
+    )
+
+    Set-FeatureFlag -Key $Key -Value $Value
+}
+$function = "Set-ModuleNameFeatureFlag"
+$destFunction = $function -replace "ModuleName", $MODULE_NAME
+if( -not (Test-Path function:$destFunction )){
+    Rename-Item -path Function:$function -NewName $destFunction
+    Export-ModuleMember -Function $destFunction
 }
